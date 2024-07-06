d3a85dc1fa0fac79333707653a5b1f5b537c314f
umashankarshah <101796759+umashankarshah@users.noreply.github.com>
2024-07-06 12:50:15 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD749jDqiKXKB_mRw1O3R79yG93fJ532ck",
+  authDomain: "react-chat-app-d6d80.firebaseapp.com",
+  databaseURL: "https://react-chat-app-d6d80-default-rtdb.firebaseio.com",
+  projectId: "react-chat-app-d6d80",
+  storageBucket: "react-chat-app-d6d80.appspot.com",
+  messagingSenderId: "458041680091",
+  appId: "1:458041680091:web:1fec1c8f900360c2a36cfe",
+  measurementId: "G-2N1RCSM3J2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ 
+    <App />
+  
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
